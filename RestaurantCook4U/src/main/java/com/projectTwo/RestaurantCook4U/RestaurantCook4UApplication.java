a014111e25f2be720d92229586469eb8a1322ef7
AnotherKate <smialkowska.katarzyna@gmail.com>
2020-05-22 21:40:13 +0200

    initial commit

---
+++ RestaurantCook4U/src/main/java/com/projectTwo/RestaurantCook4U/RestaurantCook4UApplication.java
@@ -0,0 +1,13 @@
+package com.projectTwo.RestaurantCook4U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCook4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCook4UApplication.class, args);
+	}
+
+}
---
+++ RestaurantCook4U/src/test/java/com/projectTwo/RestaurantCook4U/RestaurantCook4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectTwo.RestaurantCook4U;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCook4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
